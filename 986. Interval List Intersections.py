69408f261231418b9db28f00b574cd99890a716f
I-Lin <cuteflouer610@gmail.com>
2021-03-12 23:23:02 -0500

    two-pointer for two arrays

--- 986. Interval List Intersections.py
+++ 986. Interval List Intersections.py
@@ -1,9 +1,11 @@
 class Solution:
     def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        n1, n2 = len(A), len(B)
+        if n1 == 0 or n2 == 0: return []
         i,j = 0,0
         res = []
 
-        while i< len(A) and j < len(B):
+        while i< n1 and j < n2:
             L = max(A[i][0], B[j][0])
             H = min(A[i][1], B[j][1])
             if L <= H:
