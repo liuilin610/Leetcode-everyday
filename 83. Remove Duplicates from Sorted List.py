732eae0cc509aaa4708f489d7dc8c2a8b61d7627
I-Lin <cuteflouer610@gmail.com>
2021-03-25 22:05:39 -0400

    Linked List

--- 83. Remove Duplicates from Sorted List.py
+++ 83. Remove Duplicates from Sorted List.py
@@ -6,9 +6,22 @@
 class Solution:
     def deletDuplicates(self, head: ListNode) -> ListNode:
         if not head or not head.next: return head
+        cur = head
+        while cur and cur.next:
+            if cur.val == cur.next.val: cur.next = cur.next.next
+            else: cur = cur.next
+        return head
+        '''
+        if not head or not head.next: return head
+        head.next = self.deleteDuplicates(head.next)
+        return head.next if head.val == head.next.val else head
+        '''
+        '''
+        if head is None or head.next is None: return head
         cur, next = head, head.next
         while cur and next:
             if cur.val != next.val: cur = cur.next
             else: cur.next = next.next
             next = next.next
         return head
+        '''
