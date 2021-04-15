8fba0d5d7d5d2c022b3dc8b70fa663c8a88b762f
I-Lin <cuteflouer610@gmail.com>
2021-04-14 20:54:43 -0400

    array

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -1,12 +1,14 @@
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        n = len (intervals)
-        intervals = sorted(intervals)
-        res = [intervals[0]]
-        for i in range(1,n):
-            if (intervals[i][0] >= res[-1][1]) & (intervals[i][1] >= res[-1][1]):
-                tmp = res.pop()
-                res.append([tmp[0], intervals[i][1]])
-            if (intervals[i][0] < res[-1][1]):
-                res.append(intervals[i])
+    def merge(self, List1: List[List[int]]) -> List[List[int]]:
+        n = len(List1)
+        List.sort(key = lambda u:( u[0], -u[1]))
+        res = [List1[0]]
+
+        for i in range(1, n):
+            if ( res[-1][1] >= List1[i][0] ):
+                L, H = min(res[-1][0], List1[i][0]), max(res[-1][1], List1[i][1])
+                res.pop()
+                res.append([L, H])
+            else:
+                res.append(List1[i])
         return res
