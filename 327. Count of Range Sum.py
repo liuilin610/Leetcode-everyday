a8f9e1af8e9400f8681d014c81fcd5c5940070d7
I-Lin <cuteflouer610@gmail.com>
2021-04-23 00:29:33 -0400

    hashmap/mergesort

--- 327. Count of Range Sum.py
+++ 327. Count of Range Sum.py
@@ -17,4 +17,27 @@ class Solution:
                     res += record[psum - target]
             record[psum] += 1
         return res
-            
+
+# Time complexity: O(nlogn)
+# presum + mergesort
+class Solution: 
+    def countRangeSum ( self, nums: List[int], lower: int, upper: int) -> int:
+        presum = [0]
+        for i in nums:
+            presum.append(presum[-1] + i )
+
+        def mergesort(l, r):
+            mid = (l + r) //2
+            if ( l == mid ): return 0
+
+            ct = mergesort(l, mid) + mergesort( mid, r)
+            i = j = mid
+            for left in presum[l: mid]:
+                while ( i < r ) and ( presum[i] - left  < lower ):
+                    i += 1
+                while ( j < r ) and ( presum[j] - left <= upper ):
+                    j += 1
+                ct += j - i
+            presum[l, r] = sorted ( presum[l, r])
+            return ct
+        return mergesort(0, len(presum))
