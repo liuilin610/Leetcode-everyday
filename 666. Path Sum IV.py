bc8e49edca4f10543a6cb0547d2aa3b4b8cb0bb1
I-Lin <cuteflouer610@gmail.com>
2021-05-03 13:57:39 -0400

    dfs

---
+++ 666. Path Sum IV.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pathSum(self, nums: List[int]) -> int:
+        leaves = set( tuple( map(int, str(i)[:2] )) for i in nums )
+        Sums = {}
+
+        for i in sorted(nums):
+            a, b, c = [ map(int, i)]
+            if ( a == 1 ): Sums[(a, b)] = c
+            else:
+                parent = (a - 1, (b + 1)//2 )
+                Sums[(a, b)] = c + Sums[parent]
+                leaves.discard(parent)
+        return ( sum(Sums[k] for k in leaves))
