5f01e720dac2486902769e38f12308f45e123149
I-Lin <cuteflouer610@gmail.com>
2021-05-04 22:31:33 -0400

    dfs

---
+++ 1306. Jump Game III.py
@@ -0,0 +1,7 @@
+#Time: O(N); Space: O(1)
+class Solution:
+    def canReach(self, arr: List[int], i: int ) -> bool:
+        if ( 0 <= i < len(arr) ) and ( arr[i] >= 0 ):
+            arr[i] = -arr[i]
+            return ( arr[i] == 0 ) or ( self.canReach(arr, i + arr[i]) ) or ( self.canReach(arr, i - arr[i]) )
+        return False
