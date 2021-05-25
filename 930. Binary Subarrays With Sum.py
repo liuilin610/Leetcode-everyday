c3f811c848d88d2ff5aaa8303de9dbc68d689c32
I-Lin <cuteflouer610@gmail.com>
2021-05-25 16:00:29 -0400

    memorizaion+ presum; sliding window( two pointers)

--- 930. Binary Subarrays With Sum.py
+++ 930. Binary Subarrays With Sum.py
@@ -1,3 +1,4 @@
+#Time: O(N); Space:O(N)
 class Solution:
     def numSubarraysWithSum(self, A: List[int], S: int) -> int:
         # memorization dictionary and key '0' is for the presum = S
@@ -13,5 +14,24 @@ class Solution:
             dictA[presum] += 1
         return res
 
-        #sliding window
-        
+# Time : O(N); Space:(O(1))
+#sliding window
+# function atmost(S) to count # of subarray ( sum <= S)
+class Solution:
+    def numSubarraysWithSum(self, A: List[int], S: int) -> int:
+        def atmost(A,S):
+            if ( S < 0 ): return 0
+            res = i= 0
+            for j in range(len(A)):
+                # move of the first pointer and count the rest of sum : S- A[j]
+                S -= A[j]
+                # move the second pointer 
+                while ( S < 0 ):
+                    S += A[i]
+                    i += 1
+                res += j - i + 1
+            return res
+
+        return atmost(S) - atmost(S-1)
+
+
