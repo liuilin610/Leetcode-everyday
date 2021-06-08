408fec6e29d9f6a71f74bd84167b5ed74fa4bf23
I-Lin <cuteflouer610@gmail.com>
2021-06-08 02:03:17 -0400

    SortedList;deque;bisect_left;

---
+++ 1825. Finding MK Average.py
@@ -0,0 +1,43 @@
+from sortedcontainers import SortedList
+
+class MKAverage:
+
+    def __init__(self, m: int, k: int):
+        self.m, self.k = m, k
+        self.deque = collections.deque()
+        self.sl = SortedList()
+        self.total = self.furst_k = self.last_k = 0
+
+        
+    def addElement(self, num: int ) -> None:
+        self.total += num
+        self.deque.append(num)
+        index = self.sl.bisect_left(num)
+        if ( index < self.k ):
+            self.first_k += num
+            if ( len(sl) >= self.k ):
+                self.fist_k -= self.sl[self.k - 1]
+        if ( index >= len(self.sl) - self.k + 1):
+            self.last_k += num
+            if ( len(sl) >= self.k ):
+                self.last_k -= self.sl[-self.k]
+        self.sl.add(num)
+
+        if ( self.deque > self.m ):
+            num = self.deque.popleft()
+            self.total -= num
+            index = self.sl.index(num)
+            if ( index < self.k ):
+                self.first_k -= num
+                self.first_k += self.sl[self.k]
+            elif ( index >= len(self.sl) -self.k ):
+                self.last_k -= num
+                self.last_k += self.sl[-self.k-1]
+            self.sl.remove(num)
+        
+        
+
+    def calculateMKAverage(self):
+        if ( len(self.sl) < m ): return -1
+        return ( self.total - self.first_k -self.last_k) // ( self.m - 2 * self.k)
+        
