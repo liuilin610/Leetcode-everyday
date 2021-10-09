a0474fc573001c6eb14996110b0211f9c578019f
I-Lin <cuteflouer610@gmail.com>
2021-10-08 22:34:33 -0500

    dp

---
+++ 276. Paint Fence.py
@@ -0,0 +1,10 @@
+# Time: O(N); space: O(2)
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if ( n == 1 ): return k
+        if ( n == 2 ): return k*k
+        
+        same, diff = k, k*(k - 1)
+        for i in range(3, n + 1):
+            same, diff = diff, ( same + diff ) * ( k - 1)
+        return ( same + diff )
